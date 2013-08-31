a7dbad281db5ae6ac671c058fdaf314b24866190
Jesse Waites <jesse.waites@gmail.com>
2013-08-31 09:58:20 -0500

    converted links to buttons

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,4 @@ Flix::Application.routes.draw do
 
   root "events#index"
   resources :events
-
-  # get "events" => "events#index"
-  # get "events/:id" => "events#show", as: "event"
-  # get "events/:id/edit" => "events#edit", as: "edit_event"
-  # patch "events/:id/" => "events#update"
 end
