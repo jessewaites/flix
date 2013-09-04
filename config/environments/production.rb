fa6fdf8ae159c52b0bb64f380a508cce600a2227
Jesse Waites <jesse.waites@gmail.com>
2013-09-04 17:10:19 -0500

    ga error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Flix::Application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  GA.tracker = "UA-43688887-1"
+  #GA.tracker = "UA-43688887-1"
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
