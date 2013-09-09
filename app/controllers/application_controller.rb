390aff38a8d7be75d85b52ce694add07599a7568
Jesse Waites <jesse.waites@gmail.com>
2013-09-09 00:48:40 -0500

    added a dropcap to about page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  layout "application"
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   before_filter :authenticate_user!, except: [:index, :contact, :about, :landing, :test]
