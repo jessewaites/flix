392f38e171c948b12b40e94c9a7f4657dd9be6b3
Jesse Waites <jesse.waites@gmail.com>
2013-09-17 22:22:23 -0500

    added links to hire page

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem 'devise'
 
 
 
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Flix::Application.routes.draw do
 
 
+  
+
   devise_for :users do get '/users/sign_out' => 'devise/sessions#destroy' end
   get "info/contact"
   get "info/about"
